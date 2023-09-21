c655c21bf5a3ee186a462e58270bf903f92aa527
cvhenx <127249227+cvhenx@users.noreply.github.com>
2023-09-21 15:08:38 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ com o banco
 de dados*/
 $servername = "localhost";
 //você deu nome ao banco de dados
-$database = "bancodo2c";
+$database = "func2c";
 $username = "root";
 $password = "";
 
@@ -17,7 +17,34 @@ $conexao = mysqli_connect(
 if (!$conexao){
     die("Falha na conexão".mysqli_connect_error());
 }
-echo "conectado com sucesso";
+//echo "conectado com sucesso";
 
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$cpf = $_POST["CPF"];
+$botao = $_POST["botao"];
 
+//echo $id."  ".$nome."  ".$cpf."  ".$botao; 
+
+$sql = "INSERT INTO funcionarios (id, nome, cpf)
+    VALUES ('', '$nome', '$cpf')";
+
+if($botao == "Cadastrar"){
+    if(mysqli_query($conexao, $sql)){
+        echo "salvo com sucesso";
+    }else{
+        echo "falha ao cadastrar";
+    }
+}
 ?>
+
+<html> <body> 
+    <form name="func" method = "post">
+        <label>ID</label><input type="text" name = "id"/> </br>
+        <label>Nome</label><input type="text" name = "nome"/> </br>
+        <label>CPF</label><input type="text" name = "CPF"/> </br>
+        <input type = "submit" name = "botao" value = "Cadastrar"/>
+        <input type = "reset" name = "botao" value = "Cancelar"/>
+    </form>
+
+</body> </html> 
